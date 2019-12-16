1908429c23177489d4bfc8d6b85f67f58cfe94b9
tonyburanzon <aburanzon@mail.csuchico.edu>
2019-12-15 22:26:59 -0800

    Satisfied linter

--- src/screens/EventsResultsScreen.js
+++ src/screens/EventsResultsScreen.js
@@ -16,23 +16,23 @@ const EventsResultsScreen = ({ navigation }) => {
   for (var i in events[1]) {
     console.log(i);
   }
-  console.log(events.name)
+  console.log(events.name);
 
   return (
     // {flex: 1} constrains the View component so that it doesn't render anything out of view of the screen.
     <View style={{ flex: 1 }}>
       <Text>EventsScreen</Text>
-      <FlatList 
+      <FlatList
         data={events}
         keyExtractor={item => item.index}
-        renderItem={(item) => {
+        renderItem={item => {
           // console.log(item)
           for (var i in item) {
-            console.log(i.item)
+            console.log(i.item);
           }
           // console.log(item)
           // return <Text>{item}</Text>
-        }} 
+        }}
       />
     </View>
   );
